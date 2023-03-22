1fb5b6a9e32800fd0749dcd870b17a81494e9c7b
tsaijemmy <tsaijemmy@gmail.com>
2023-03-22 11:49:26 +0800

    Add Calcatuor.cs

---
+++ ProGitForProgrammers/Calcuator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProGitForProgrammers {
+
+    class Calcuator 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! World!");
+        }
+    }
+}
