75549c8fac3bf99319d3477b174ab84ecee08b06
tsaijemmy <tsaijemmy@gmail.com>
2023-03-22 17:23:31 +0800

    Add Boock Class

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProGitForProgrammers {
+
+    class Book 
+    {
+        public string Tile {get; set;}
+        public List<string> Authors {get; set;}
+
+        public DateTime PublishDate { get; set;}
+    }
+}
